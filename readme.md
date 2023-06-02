dcd4b379887cc841274b48dae39746fb91fa3d69
OfficiallyDragon <dexterwhetcroft@gmail.com>
2023-06-02 12:59:00 +0100

    Renamed Project to ISC: Increased SMP Core Instead of OSC: Otto SMP Core

---
+++ readme.md
@@ -0,0 +1,3 @@
+<center> <h1>ISC</h1> 
+<h3>The official Repository for Increased SMP Core</h3>
+</center>
---
+++ src/main/java/dev/dragongames/increasedsmpcore/ISC.java
@@ -0,0 +1,17 @@
+package dev.dragongames.increasedsmpcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ISC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
