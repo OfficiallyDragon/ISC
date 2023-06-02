0e70fd8788cb321021d0ad173aec85dd2c79618a
OfficiallyDragon <dexterwhetcroft@gmail.com>
2023-06-02 14:21:05 +0100

    Added log messages for starting and stopping the plugin!

--- src/main/java/dev/dragongames/increasedsmpcore/ISC.java
+++ src/main/java/dev/dragongames/increasedsmpcore/ISC.java
@@ -2,16 +2,23 @@ package dev.dragongames.increasedsmpcore;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public final class ISC extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
 
+        getLogger().info("ISC Has started!");
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
+        getLogger().info("ISC Has stopped.");
+
     }
 }
